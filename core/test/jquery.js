2cb981c78c3f98f623918fb71748137e7cc48e59
Scott González <scott.gonzalez@gmail.com>
2011-02-14 17:00:38 -0500

    Default to jQuery 1.5 for tests.

--- core/test/jquery.js
+++ core/test/jquery.js
@@ -4,7 +4,7 @@ var parts = document.location.search.slice( 1 ).split( "&" ),
 	length = parts.length,
 	i = 0,
 	current,
-	version = "1.4.4";
+	version = "1.5";
 
 for ( ; i < length; i++ ) {
 	current = parts[ i ].split( "=" );
