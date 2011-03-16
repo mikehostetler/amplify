0d675025d4015e6ce805ef1a39008bfd00356416
Scott González <scott.gonzalez@gmail.com>
2011-03-16 16:15:35 -0400

    Added jQuery 1.5.1.

--- core/test/jquery.js
+++ core/test/jquery.js
@@ -18,7 +18,7 @@ for ( ; i < length; i++ ) {
 if ( version === "git" ) {
 	url = "http://code.jquery.com/jquery-git.js";
 } else {
-	url = "../../external/jquery-" + ( version || "1.5" ) + ".js";
+	url = "../../external/jquery-" + ( version || "1.5.1" ) + ".js";
 }
 
 document.write( "<script src='" + url + "'></script>" );
