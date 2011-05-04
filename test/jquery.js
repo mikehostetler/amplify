317bbf016db3d228eea27f00c807fa0b074e308f
Scott González <scott.gonzalez@gmail.com>
2011-05-04 17:36:39 -0400

    Added jQuery 1.6.

--- test/jquery.js
+++ test/jquery.js
@@ -18,7 +18,7 @@ for ( ; i < length; i++ ) {
 if ( version === "git" ) {
 	url = "http://code.jquery.com/jquery-git.js";
 } else {
-	url = "../../external/jquery-" + ( version || "1.5.2" ) + ".js";
+	url = "../../external/jquery-" + ( version || "1.6" ) + ".js";
 }
 
 document.write( "<script src='" + url + "'></script>" );
