68d6b6e5cd006363bd63e2ac34b9c5a11b8d6659
Scott González <scott.gonzalez@gmail.com>
2011-11-02 14:44:14 -0400

    Store: Use a try/catch to detect if userData exists. Hopefully fixes #17.

--- store/amplify.store.js
+++ store/amplify.store.js
@@ -153,65 +153,88 @@ if ( window.globalStorage ) {
 		attrKey = "amplify";
 	div.style.display = "none";
 	document.getElementsByTagName( "head" )[ 0 ].appendChild( div );
-	if ( div.addBehavior ) {
+
+	// we can't feature detect userData support
+	// so just try and see if it fails
+	// surprisingly, even just adding the behavior isn't enough for a failure
+	// so we need to load the data as well
+	try {
 		div.addBehavior( "#default#userdata" );
+		div.load( attrKey );
+	} catch( e ) {
+		div.parentNode.removeChild( div );
+		return;
+	}
 
-		store.addType( "userData", function( key, value, options ) {
-			div.load( attrKey );
-			var attr, parsed, prevValue, i, remove,
-				ret = value,
-				now = (new Date()).getTime();
+	store.addType( "userData", function( key, value, options ) {
+		div.load( attrKey );
+		var attr, parsed, prevValue, i, remove,
+			ret = value,
+			now = (new Date()).getTime();
 
-			if ( !key ) {
-				ret = {};
-				remove = [];
-				i = 0;
-				while ( attr = div.XMLDocument.documentElement.attributes[ i++ ] ) {
-					parsed = JSON.parse( attr.value );
-					if ( parsed.expires && parsed.expires <= now ) {
-						remove.push( attr.name );
-					} else {
-						ret[ attr.name ] = parsed.data;
-					}
-				}
-				while ( key = remove.pop() ) {
-					div.removeAttribute( key );
-				}
-				div.save( attrKey );
-				return ret;
-			}
-
-			// convert invalid characters to dashes
-			// http://www.w3.org/TR/REC-xml/#NT-Name
-			// simplified to assume the starting character is valid
-			// also removed colon as it is invalid in HTML attribute names
-			key = key.replace( /[^-._0-9A-Za-z\xb7\xc0-\xd6\xd8-\xf6\xf8-\u037d\u37f-\u1fff\u200c-\u200d\u203f\u2040\u2070-\u218f]/g, "-" );
-
-			if ( value === undefined ) {
-				attr = div.getAttribute( key );
-				parsed = attr ? JSON.parse( attr ) : { expires: -1 };
+		if ( !key ) {
+			ret = {};
+			remove = [];
+			i = 0;
+			while ( attr = div.XMLDocument.documentElement.attributes[ i++ ] ) {
+				parsed = JSON.parse( attr.value );
 				if ( parsed.expires && parsed.expires <= now ) {
-					div.removeAttribute( key );
+					remove.push( attr.name );
 				} else {
-					return parsed.data;
-				}
-			} else {
-				if ( value === null ) {
-					div.removeAttribute( key );
-				} else {
-					// we need to get the previous value in case we need to rollback
-					prevValue = div.getAttribute( key );
-					parsed = JSON.stringify({
-						data: value,
-						expires: (options.expires ? (now + options.expires) : null)
-					});
-					div.setAttribute( key, parsed );
+					ret[ attr.name ] = parsed.data;
 				}
 			}
+			while ( key = remove.pop() ) {
+				div.removeAttribute( key );
+			}
+			div.save( attrKey );
+			return ret;
+		}
 
+		// convert invalid characters to dashes
+		// http://www.w3.org/TR/REC-xml/#NT-Name
+		// simplified to assume the starting character is valid
+		// also removed colon as it is invalid in HTML attribute names
+		key = key.replace( /[^-._0-9A-Za-z\xb7\xc0-\xd6\xd8-\xf6\xf8-\u037d\u37f-\u1fff\u200c-\u200d\u203f\u2040\u2070-\u218f]/g, "-" );
+
+		if ( value === undefined ) {
+			attr = div.getAttribute( key );
+			parsed = attr ? JSON.parse( attr ) : { expires: -1 };
+			if ( parsed.expires && parsed.expires <= now ) {
+				div.removeAttribute( key );
+			} else {
+				return parsed.data;
+			}
+		} else {
+			if ( value === null ) {
+				div.removeAttribute( key );
+			} else {
+				// we need to get the previous value in case we need to rollback
+				prevValue = div.getAttribute( key );
+				parsed = JSON.stringify({
+					data: value,
+					expires: (options.expires ? (now + options.expires) : null)
+				});
+				div.setAttribute( key, parsed );
+			}
+		}
+
+		try {
+			div.save( attrKey );
+		// quota exceeded
+		} catch ( error ) {
+			// roll the value back to the previous value
+			if ( prevValue === null ) {
+				div.removeAttribute( key );
+			} else {
+				div.setAttribute( key, prevValue );
+			}
+
+			// expire old data and try again
+			store.userData();
 			try {
+				div.setAttribute( key, parsed );
 				div.save( attrKey );
-			// quota exceeded
 			} catch ( error ) {
 				// roll the value back to the previous value
 				if ( prevValue === null ) {
@@ -219,25 +242,11 @@ if ( window.globalStorage ) {
 				} else {
 					div.setAttribute( key, prevValue );
 				}
-
-				// expire old data and try again
-				store.userData();
-				try {
-					div.setAttribute( key, parsed );
-					div.save( attrKey );
-				} catch ( error ) {
-					// roll the value back to the previous value
-					if ( prevValue === null ) {
-						div.removeAttribute( key );
-					} else {
-						div.setAttribute( key, prevValue );
-					}
-					throw store.error();
-				}
+				throw store.error();
 			}
-			return ret;
-		});
-	}
+		}
+		return ret;
+	});
 }() );
 
 // in-memory storage
