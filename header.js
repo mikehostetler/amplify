f1a2814eaa47bc60ecefa81f2b0f4c3e05fe77ec
Elijah Manor <elijah.manor@gmail.com>
2012-09-24 20:38:06 -0500

    Add build files for anvil

---
+++ header.js
@@ -0,0 +1,10 @@
+/*
+	{{{name}}} - {{{description}}}
+	version:	{{{version}}}
+	author:		{{{author}}}
+	copyright:	2011 - 2012
+	license:	Dual licensed (http://appendto.com/open-source-licenses)
+				MIT (http://www.opensource.org/licenses/mit-license)
+				GPL (http://www.opensource.org/licenses/gpl-license)
+	website:    http://amplifyjs.com
+*/
