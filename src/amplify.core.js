56ff2440353d192f085b7a851ad8ba6431c6bdda
Elijah Manor <elijah.manor@gmail.com>
2012-11-05 10:56:48 -0600

    Add some options to the build file

--- src/amplify.core.js
+++ src/amplify.core.js
@@ -1,6 +1,7 @@
 /*!
+ * license
  * Amplify Core {{{version}}}
- *
+ * {{{hello}}}
  * Copyright 2011 appendTo LLC. (http://appendto.com/team)
  * Dual licensed under the MIT or GPL licenses.
  * http://appendto.com/open-source-licenses
